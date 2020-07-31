0dd8984799bb6808fae9e4c08bb88ed610acb520
Gus <amazing951amazing951@gmail.com>
2020-07-31 20:46:43 +0800

    login page

--- week6_router/src/views/Home.vue
+++ week6_router/src/views/Home.vue
@@ -5,7 +5,7 @@
       <router-link to="/about">About</router-link> |
       <router-link to="/products">前台產品葉面</router-link> |
       <router-link to="/cart">狗屋車</router-link> |
-      <router-link to="/admin">後台葉面</router-link>
+      <router-link to="/login">後台葉面</router-link>
       <router-view />
   </div>
 </template>
